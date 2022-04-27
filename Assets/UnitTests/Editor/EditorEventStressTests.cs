2420666771ccf420e7ff544a0ab5e69436d74901
Steffen Itterheim <42713966+sitterheim@users.noreply.github.com>
2022-04-27 12:20:32 +0200

    added Editor unit tests with performance measurements of systems A-J

---
+++ Assets/UnitTests/Editor/EditorEventStressTests.cs
@@ -0,0 +1,103 @@
+using NUnit.Framework;
+using TestHelper;
+using Unity.Entities;
+using Unity.PerformanceTesting;
+using Unity.Transforms;
+
+/// <summary>
+/// To run performance measurements:
+/// - open StressTest scene
+/// - open: Window => Analysis => Performance Test Report
+/// - open: Window => General => Test Runner
+/// Run tests in Test Runner. Then check Performance Test Report window.
+/// </summary>
+public class EditorEventStressTests : ECSTestsFixture
+{
+	// TODO: move these into a ScriptableObject for easier/faster value tweaking
+	private readonly int _entityCount = 500000;
+	private readonly float _spacing = 1f;
+	private readonly int _damagers = 1;
+
+	private void MeasureWorldUpdate(EventType eventType)
+	{
+		// create the HealthPrefab entity
+		// NOTE: I found it difficult to verify whether this is 100% identical to the scene's Health prefab entity after conversion.
+		// I add the systems from the Archetypes listed for HealthPrefab in play mode as well as the conversion code.
+		// Should be identical but it's kind of hard to use the DOTS windows while tests are running whereas with breakpoints the UI is frozen.
+		var healthPrefab = m_Manager.CreateEntity(typeof(LocalToWorld), typeof(Translation), typeof(Rotation), typeof(Health), typeof(Prefab));
+		m_Manager.AddComponentData(healthPrefab, new Health { Value = 1000f });
+		m_Manager.AddBuffer<DamageEvent>(healthPrefab);
+
+		// we need this to spawn all the entities during the first world update
+		var spawner = m_Manager.CreateEntity(typeof(EventStressTest));
+		m_Manager.SetComponentData(spawner, CreateEventStressTest(eventType, healthPrefab));
+
+		Measure.Method(() =>
+			{
+				// update the world once, running all systems once
+				m_World.Update();
+
+				// this did not seem to affect measurements (by much) but it's better to be safe,
+				// we don't want any jobs to continue running past the measurement cycle
+				m_Manager.CompleteAllJobs();
+			})
+			// First update creates gazillion entities, don't measure this... (actually: could call Update outside Measure once)
+			// Second update seems generally unstable, skip that too ...
+			// From third run onwards measurements are stable ...
+			.WarmupCount(2)
+			// 10 seems enough to get a decently low deviation
+			.MeasurementCount(10)
+			// only measure once to keep numbers comparable to original forum post
+			.IterationsPerMeasurement(1)
+			.Run();
+	}
+
+	private EventStressTest CreateEventStressTest(EventType eventType, Entity prefab) => new EventStressTest
+	{
+		EventType = eventType,
+		HealthPrefab = prefab,
+		HealthEntityCount = _entityCount,
+		Spacing = _spacing,
+		DamagersPerHealths = _damagers,
+	};
+
+	[Test, Performance]
+	public void A_ParallelWriteToStream_ParallelPollBuffers() => MeasureWorldUpdate(EventType.A_ParallelWriteToStream_ParallelPollBuffers);
+
+	[Test, Performance]
+	public void B_SingleWriteToBuffers_ParallelPollBuffers() => MeasureWorldUpdate(EventType.B_SingleWriteToBuffers_ParallelPollBuffers);
+
+	[Test, Performance]
+	public void C_ParallelWriteToBuffersECB_ParallelPollBuffers() => MeasureWorldUpdate(EventType.C_ParallelWriteToBuffersECB_ParallelPollBuffers);
+
+	[Test, Performance]
+	public void D_ParallelWriteToStream_SingleApplyToEntities() => MeasureWorldUpdate(EventType.D_ParallelWriteToStream_SingleApplyToEntities);
+
+	[Test, Performance]
+	public void E_ParallelWriteToStream_ParallelApplyToEntities() => MeasureWorldUpdate(EventType.E_ParallelWriteToStream_ParallelApplyToEntities);
+
+	[Test, Performance]
+	public void F_ParallelCreateEventEntities_SingleApplyToEntities() => MeasureWorldUpdate(EventType.F_ParallelCreateEventEntities_SingleApplyToEntities);
+
+	[Test, Performance]
+	public void G_ParallelWriteToStream_SinglePollList() => MeasureWorldUpdate(EventType.G_ParallelWriteToStream_SinglePollList);
+
+	[Test, Performance]
+	public void H_ParallelWriteToStream_SinglePollHashMap() => MeasureWorldUpdate(EventType.H_ParallelWriteToStream_SinglePollHashMap);
+
+	[Test, Performance]
+	public void I_ParallelWriteToStream_ParallelPollHashMap() => MeasureWorldUpdate(EventType.I_ParallelWriteToStream_ParallelPollHashMap);
+
+	[Test, Performance]
+	public void J_SingleDirectModification() => MeasureWorldUpdate(EventType.J_SingleDirectModification);
+
+	public override void Setup()
+	{
+		// custom flag in ECSTestsFixture that allows creating the default world, rather than an empty (no systems) world
+		// must be set before base.Setup() !
+		CreateDefaultWorld = true;
+
+		// setup the Entities world
+		base.Setup();
+	}
+}
---
+++ Assets/UnitTests/Runtime/RuntimeEventStressTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using TestHelper;
+using Unity.Entities;
+using Unity.PerformanceTesting;
+using Unity.Transforms;
+
+public partial class RuntimeEventStressTests : ECSTestsFixture
+{
+    [Test, Performance]
+    public void EntitiesTestExample()
+    {
+    }
+}
